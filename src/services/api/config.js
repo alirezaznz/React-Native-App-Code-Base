c708c1e9842490c57a3ce5ffa1761e19b2a52bfc
Farhad <af.kianinejad@gmial.com>
2022-05-01 16:34:22 +0430

    config axios cache adaptor

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -34,4 +34,42 @@ API.interceptors.response.use(
     }
 );
 
-export { API };
+// Create `axios-cache-adapter` instance
+const cache = setupCache({
+    maxAge: 15 * 60 * 1000
+})
+
+const CacheAPI = axios.create({
+    baseURL: ENV.baseURL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    adapter: cache.adapter
+});
+
+CacheAPI.interceptors.request.use(
+    (config) => {
+        ////ebugger
+        return config;
+    },
+    function (err) {
+        // handle error
+        return Promise.reject(err);
+    }
+);
+
+CacheAPI.interceptors.response.use(
+    function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+    },
+    function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        HandleAPIErrorPayload(error);
+        return Promise.reject(error);
+    }
+);
+
+export { API, CacheAPI };

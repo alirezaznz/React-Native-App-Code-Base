2fdb1203639409c5facae8a63e308084e2743fe5
alirezaznz <alireza.nz11@gmail.com>
2022-05-05 16:27:50 +0430

    fixed jsx related problem in metro config

--- metro.config.js
+++ metro.config.js
@@ -6,12 +6,15 @@
  */
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
+    resolver: {
+        sourceExts: ['jsx', 'js', 'json', 'ts', 'tsx'],
+    },
 };

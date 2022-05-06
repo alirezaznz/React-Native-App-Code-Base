3dad71f9f604457e94185e34ec1b2e52747d007a
alirezaznz <alireza.nz11@gmail.com>
2022-05-06 13:29:13 +0430

    converted bimetrics and networkStateChange to tsx and edited eslint configs to ignore variable declartion error

--- src/components/NetworkStateCheck.tsx
+++ src/components/NetworkStateCheck.tsx
@@ -1,16 +1,6 @@
-import NetInfo, {useNetInfo} from '@react-native-community/netinfo';
-import React, {useEffect, useState} from 'react';
-import {
-    Text,
-    ActivityIndicator,
-    Modal,
-    StyleSheet,
-    Dimensions,
-    I18nManager,
-    Platform,
-    View,
-} from 'react-native';
-import {ENV} from '@Constants';
+import NetInfo from '@react-native-community/netinfo';
+import React, {FC, useEffect, useState} from 'react';
+import {Text, Modal, StyleSheet, View} from 'react-native';
 
 NetInfo.configure({
     reachabilityUrl: 'https://google.com',
@@ -19,17 +9,12 @@ NetInfo.configure({
     reachabilityTest: async response => response.status === 200,
     reachabilityShouldRun: () => true,
 });
-
 // check locale
-export const NetworkStateCheck = ({navigation}) => {
+export const NetworkStateCheck: FC = () => {
     const [modalVisible, setModalVisible] = useState(false);
-
-    const {isConnected, isInternetReachable} = useNetInfo();
-
     useEffect(() => {
         // Subscribe
-        const unsubscribe = NetInfo.addEventListener(state => {
-            // debugger
+        const listener = NetInfo.addEventListener(state => {
             if (
                 state.isConnected === false ||
                 state.isInternetReachable === false
@@ -40,7 +25,7 @@ export const NetworkStateCheck = ({navigation}) => {
             }
         });
         return () => {
-            //unsubscribe();
+            listener();
         };
     }, []);
 
@@ -52,9 +37,6 @@ export const NetworkStateCheck = ({navigation}) => {
         </Modal>
     );
 };
-
-const stepSize = Platform.select({ios: 70, android: 60});
-const {width, height} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     modalView: {
